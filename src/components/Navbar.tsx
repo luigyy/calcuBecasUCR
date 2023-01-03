a629988bfbda8f1da5c0e9f72d890c5daa6fbf0b
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-02 21:46:16 -0600

    added contact icons to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,44 @@
 import React from "react";
+import { BsGithub, BsInstagram } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = ({}) => {
   return (
     <div>
-      <div className="navbar w-[95%] flex justify-center bg-base-100 m-2 shadow mx-auto">
-        <span className="-tracking-wider font-semibold normal-case md:text-3xl text-xl">
-          calcuBecas<span className="text-primary">UCR</span>
-        </span>
+      <div className="navbar w-[95%] flex md:px-20 justify-between bg-base-100 m-2 shadow mx-auto">
+        {/* logo */}
+        <div>
+          <span className="-tracking-wider font-semibold normal-case md:text-3xl text-xl">
+            calcuBecas<span className="text-primary">UCR</span>
+          </span>
+        </div>
+        {/* logo */}
+        <div className="flex gap-5">
+          <h1
+            className="tooltip tooltip-bottom text-2xl"
+            data-tip="luigy.valverde@ucr.ac.cr"
+          >
+            {<AiOutlineMail />}
+          </h1>
+          <a
+            href="https://github.com/luigyy/calcuBecasUCR"
+            target="_blank"
+            className="tooltip tooltip-bottom hover:shadow-2xl  text-2xl"
+            data-tip="Colabora"
+          >
+            {<BsGithub />}
+          </a>
+          <a
+            href="https://www.instagram.com/luigyyyyyy/"
+            target="_blank"
+            className="text-2xl tooltip tooltip-bottom"
+            data-tip="Mi contacto"
+          >
+            {<BsInstagram />}
+          </a>
+        </div>
       </div>
     </div>
   );
